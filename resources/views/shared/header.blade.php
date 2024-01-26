730f257367dbbed24da3a522ddc8bf1935774508
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-01-26 15:32:43 +0100

    feat: header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,3 +1,19 @@
 <header>
-    header
+    <!-- Logo -->
+    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo">
+    <!-- Fine Logo -->
+    <!-- Menu -->
+    <ul>
+        <li><a href="#">characters</a></li>
+        <li class="selected"><a href="#">comics</a></li>
+        <li><a href="#">movies</a></li>
+        <li><a href="#">tv</a></li>
+        <li><a href="#">games</a></li>
+        <li><a href="#">collectibles</a></li>
+        <li><a href="#">videos</a></li>
+        <li><a href="#">fans</a></li>
+        <li><a href="#">news</a></li>
+        <li><a href="#">shop</a></li>
+    </ul>
+    <!-- Fine Menu -->
 </header>
